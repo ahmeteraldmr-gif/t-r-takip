4a20f0a909c5a8e4a31f876c552562507a81af56
ahmeteraldmr-gif <ahmeteraldmr@gmail.com>
2026-02-28 02:50:19 +0300

    css hatası

--- database/migrations/2025_02_16_000006_add_first_name_last_name_to_users_table.php
+++ database/migrations/2025_02_16_000006_add_first_name_last_name_to_users_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
@@ -25,7 +24,8 @@ return new class extends Migration
         }
 
         Schema::table('users', function (Blueprint $table) {
-            $table->string('first_name')->nullable(false)->change();
+            $table->string('first_name')->nullable()->change();
+            $table->string('last_name')->nullable()->change();
         });
     }
 
